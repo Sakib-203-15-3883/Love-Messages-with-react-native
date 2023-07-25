f5d323b497216728d089ce144a006f715e0c3a8a
Sakib all hasan <sakibul15.3883@gmail.com>
2023-07-25 16:46:43 +0600

    update top and bottom part

--- Navigation.js
+++ Navigation.js
@@ -1,24 +1,78 @@
-// Navigation.js
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Provider as PaperProvider} from 'react-native-paper';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Text } from 'react-native'; // Add this import
 import HomeScreen from './screens/HomeScreen';
-
-import AboutScreen from './screens/AboutScreen';
 import Plan1 from './screens/Plan1';
 import Plan2 from './screens/Plan2';
 import Plan3 from './screens/Plan3';
 import Plan4 from './screens/Plan4';
+import AboutScreen from './screens/AboutScreen';
 
 const Tab = createBottomTabNavigator();
+
+const getTabBarIcon = (route, focused) => {
+  let iconName;
+  switch (route.name) {
+    case 'Home':
+      iconName = focused ? '🏠' : '🏚️';
+      break;
+    case ' Random girls':
+      iconName = focused ? '👩‍🌾' : '👩‍🌾';
+      break;
+    case ' Senior':
+      iconName = focused ? '👩‍💻' : '👩‍💻';
+      break;
+    case ' Junior':
+      iconName = focused ? '👩‍🎤' : '👩‍🎤';
+      break;
+    case ' classMate':
+      iconName = focused ? '👫' : '👭';
+      break;
+    case 'About':
+      iconName = focused ? '💀' : '💀';
+      break;
+    default:
+      iconName = '';
+  }
+  return <Text style={{ fontSize: 24 }}>{iconName}</Text>;
+};
+
+const getHeaderTitle = (route) => {
+  switch (route.name) {
+    case 'Home':
+      return 'Home Screen 🏠';
+    case ' Random girls':
+      return ' 🤡Impress Random Girls/Boys 👩‍🦰';
+    case ' Senior':
+      return ' 😻😻Impress Senior Girls/Boys🤑';
+    case ' Junior':
+      return '  ☘️Impress Junior Girls/Boys🙃🔥';
+    case ' classMate':
+      return '   🤭🥴Impress ClassMate👫👻';
+    case 'About':
+      return 'About Screen ℹ️';
+    default:
+      return '';
+  }
+};
+
 
 const Navigation = () => {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          tabBarOptions={{
+            tabStyle: { paddingVertical: -20, height: 50 }, // Adjust the padding and height as needed
+            labelStyle: { fontSize: 11 }, // Adjust the font size as needed
+          }}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => getTabBarIcon(route, focused),
+            headerTitle: getHeaderTitle(route),
+          })}
+        >
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name=" Random girls" component={Plan1} />
           <Tab.Screen name=" Senior" component={Plan2} />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,41 +1,27 @@
-// HomeScreen.js
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-const HomeScreen = ({ navigation }) => {
-  const goToPlan1 = () => {
-    navigation.navigate('Plan1');
-  };
-
+const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Home Screen!</Text>
-      <TouchableOpacity style={styles.button} onPress={goToPlan1}>
-        <Text style={styles.buttonText}>Go to Plan1</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={['#4e54c8', '#8f94fb']} style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome to the Home Screen!</Text>
+      </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: '#4e54c8',
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
+
+  text: {
+    color: 'black',
+    fontSize: 29,
   },
 });
 
